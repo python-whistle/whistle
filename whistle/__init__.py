e0f02ec4c91a55941f223f58f3bb99e1c3b856c2
Romain Dorgueil <romain@dorgueil.net>
2017-10-17 07:23:44 +0200

    release: 1.0a2

--- whistle/__init__.py
+++ whistle/__init__.py
@@ -1,1 +1,8 @@
-from __future__ import absolute_import, print_function, unicode_literals
+# coding: utf-8
+
+from __future__ import absolute_import, unicode_literals, print_function
+
+from edgy.event.event import Event
+from edgy.event.dispatcher import EventDispatcher
+
+__all__ = ['Event', 'EventDispatcher']
--- whistle/event/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import, unicode_literals, print_function
-
-from edgy.event.event import Event
-from edgy.event.dispatcher import EventDispatcher
-
-__all__ = ['Event', 'EventDispatcher']
