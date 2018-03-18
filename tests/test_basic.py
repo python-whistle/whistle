f1f6320dd10530661c076d106060cd6649c11548
Romain Dorgueil <romain@dorgueil.net>
2018-03-18 12:07:58 +0100

    Tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
+from unittest import mock
 
-from unittest import TestCase
-try:
-    from unittest import mock
-except ImportError:
-    import mock
+import pytest
 
 from whistle import Event, EventDispatcher
 
@@ -13,135 +9,153 @@ OBJECT_EVENT_ID = object()
 NUMERIC_EVENT_ID = 42
 
 
-class BasicTest(TestCase):
-    def test_event(self):
-        event = Event()
-        assert not event.propagation_stopped
+class CustomEvent(Event):
+    pass
 
-        event.stop_propagation()
-        assert event.propagation_stopped
 
-    def test_dispatcher(self):
-        dispatcher = EventDispatcher()
+def test_event():
+    event = Event()
+    assert not event.propagation_stopped
 
-        for event_id in (
+    event.stop_propagation()
+    assert event.propagation_stopped
+
+
+@pytest.mark.parametrize('event_id', [STRING_EVENT_ID, OBJECT_EVENT_ID, NUMERIC_EVENT_ID])
+def test_dispatcher(event_id):
+    dispatcher = EventDispatcher()
+
+    listener = mock.MagicMock()
+    assert not dispatcher.has_listeners(event_id)
+    dispatcher.add_listener(event_id, listener)
+    assert dispatcher.has_listeners(event_id)
+
+    assert listener.call_count == 0
+
+    e = dispatcher.dispatch(event_id)
+
+    assert listener.call_count == 1
+    assert not e.propagation_stopped
+
+    e = dispatcher.dispatch(event_id)
+
+    assert listener.call_count == 2
+    assert not e.propagation_stopped
+
+
+@pytest.mark.parametrize('EventType', [Event, CustomEvent])
+@pytest.mark.parametrize('event_id', [STRING_EVENT_ID, OBJECT_EVENT_ID, NUMERIC_EVENT_ID])
+def test_dispatcher_custom_event(EventType, event_id):
+    dispatcher = EventDispatcher()
+    listener = mock.MagicMock()
+    event = EventType()
+
+    dispatcher.add_listener(event_id, listener)
+    e = dispatcher.dispatch(event_id, event)
+
+    assert listener.call_count == 1
+    assert e == event
+
+
+def test_propagation():
+    dispatcher = EventDispatcher()
+
+    for event_id in (
             STRING_EVENT_ID,
             OBJECT_EVENT_ID,
             NUMERIC_EVENT_ID,
-        ):
-            listener = mock.MagicMock()
-            assert not dispatcher.has_listeners(event_id)
-            dispatcher.add_listener(event_id, listener)
-            assert dispatcher.has_listeners(event_id)
+    ):
+        listener1 = mock.MagicMock()
 
-            assert listener.call_count == 0
+        def listener2(event):
+            event.stop_propagation()
 
-            e = dispatcher.dispatch(event_id)
+        listener3 = mock.MagicMock()
 
-            assert listener.call_count == 1
-            assert not e.propagation_stopped
+        dispatcher.add_listener(event_id, listener1)
+        dispatcher.add_listener(event_id, listener2)
+        dispatcher.add_listener(event_id, listener3)
 
-            e = dispatcher.dispatch(event_id)
+        assert listener1.call_count == 0
+        assert listener3.call_count == 0
 
-            assert listener.call_count == 2
-            assert not e.propagation_stopped
+        e = dispatcher.dispatch(event_id)
 
-    def test_propagation(self):
-        dispatcher = EventDispatcher()
+        assert listener1.call_count == 1
+        assert listener3.call_count == 0
+        assert e.propagation_stopped
 
-        for event_id in (
+        e = dispatcher.dispatch(event_id)
+
+        assert listener1.call_count == 2
+        assert listener3.call_count == 0
+        assert e.propagation_stopped
+
+        assert dispatcher.get_listeners(event_id) == [listener1, listener2, listener3]
+
+    listeners = dispatcher.get_listeners()
+    assert len(listeners) == 3
+    for event_id in (
             STRING_EVENT_ID,
             OBJECT_EVENT_ID,
             NUMERIC_EVENT_ID,
-        ):
-            listener1 = mock.MagicMock()
+    ):
+        assert len(listeners[event_id]) == 3
 
-            def listener2(event):
-                event.stop_propagation()
 
-            listener3 = mock.MagicMock()
-
-            dispatcher.add_listener(event_id, listener1)
-            dispatcher.add_listener(event_id, listener2)
-            dispatcher.add_listener(event_id, listener3)
-
-            assert listener1.call_count == 0
-            assert listener3.call_count == 0
-
-            e = dispatcher.dispatch(event_id)
-
-            assert listener1.call_count == 1
-            assert listener3.call_count == 0
-            assert e.propagation_stopped
-
-            e = dispatcher.dispatch(event_id)
-
-            assert listener1.call_count == 2
-            assert listener3.call_count == 0
-            assert e.propagation_stopped
-
-            self.assertSequenceEqual(dispatcher.get_listeners(event_id), (listener1, listener2, listener3))
-
-        listeners = dispatcher.get_listeners()
-        assert len(listeners) == 3
-        for event_id in (
+def test_no_listener():
+    dispatcher = EventDispatcher()
+    for event_id in (
             STRING_EVENT_ID,
             OBJECT_EVENT_ID,
             NUMERIC_EVENT_ID,
-        ):
-            assert len(listeners[event_id]) == 3
+    ):
+        assert not dispatcher.has_listeners(event_id)
 
-    def test_no_listener(self):
-        dispatcher = EventDispatcher()
-        for event_id in (
+        e = dispatcher.dispatch(event_id)
+        assert not e.propagation_stopped
+
+
+def test_remove_listener():
+    dispatcher = EventDispatcher()
+
+    for event_id in (
             STRING_EVENT_ID,
             OBJECT_EVENT_ID,
             NUMERIC_EVENT_ID,
-        ):
-            assert not dispatcher.has_listeners(event_id)
+    ):
+        listener = mock.MagicMock()
+        assert not dispatcher.has_listeners(event_id)
+        dispatcher.add_listener(event_id, listener)
+        assert dispatcher.has_listeners(event_id)
 
-            e = dispatcher.dispatch(event_id)
-            assert not e.propagation_stopped
+        assert listener.call_count == 0
 
-    def test_remove_listener(self):
-        dispatcher = EventDispatcher()
+        e = dispatcher.dispatch(event_id)
 
-        for event_id in (
+        assert listener.call_count == 1
+        assert not e.propagation_stopped
+
+        assert dispatcher.has_listeners(event_id)
+        dispatcher.remove_listener(event_id, listener)
+        assert not dispatcher.has_listeners(event_id)
+
+        e = dispatcher.dispatch(event_id)
+
+        assert listener.call_count == 1
+        assert not e.propagation_stopped
+
+
+def test_listen_decorator():
+    dispatcher = EventDispatcher()
+
+    for event_id in (
             STRING_EVENT_ID,
             OBJECT_EVENT_ID,
             NUMERIC_EVENT_ID,
-        ):
-            listener = mock.MagicMock()
-            assert not dispatcher.has_listeners(event_id)
-            dispatcher.add_listener(event_id, listener)
-            assert dispatcher.has_listeners(event_id)
-
-            assert listener.call_count == 0
-
-            e = dispatcher.dispatch(event_id)
-
-            assert listener.call_count == 1
-            assert not e.propagation_stopped
-
-            assert dispatcher.has_listeners(event_id)
-            dispatcher.remove_listener(event_id, listener)
-            assert not dispatcher.has_listeners(event_id)
-
-            e = dispatcher.dispatch(event_id)
-
-            assert listener.call_count == 1
-            assert not e.propagation_stopped
-
-    def test_listen_decorator(self):
-        dispatcher = EventDispatcher()
-
-        for event_id in (
-            STRING_EVENT_ID,
-            OBJECT_EVENT_ID,
-            NUMERIC_EVENT_ID,
-        ):
-            listener = mock.MagicMock()
-            dispatcher.listen(event_id)(listener)
-            e = dispatcher.dispatch(event_id)
-            assert listener.call_count == 1
-            assert not e.propagation_stopped
+    ):
+        listener = mock.MagicMock()
+        dispatcher.listen(event_id)(listener)
+        e = dispatcher.dispatch(event_id)
+        assert listener.call_count == 1
+        assert not e.propagation_stopped
