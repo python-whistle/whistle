ff22908857c438c3f7a7e50369005f960bb1c8d8
Romain Dorgueil <romain@makersquad.fr>
2024-01-01 03:05:46 +0100

    feat: root level interfaces

--- whistle/__init__.py
+++ whistle/__init__.py
@@ -1,9 +1,14 @@
 from whistle._version import __version__
-from whistle.dispatcher import EventDispatcher
+from whistle.dispatcher import AsyncEventDispatcher, EventDispatcher
 from whistle.event import Event
+from whistle.protocols import IAsyncEventDispatcher, IEvent, IEventDispatcher
 
 __all__ = [
+    "AsyncEventDispatcher",
     "Event",
     "EventDispatcher",
+    "IAsyncEventDispatcher",
+    "IEvent",
+    "IEventDispatcher",
     "__version__",
 ]
