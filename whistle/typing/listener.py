54d823e54e7113a5f7c4f25dbe91ae9837c1d448
Romain Dorgueil <romain@makersquad.fr>
2025-10-24 10:42:46 +0200

    chore: fixing lint

--- whistle/typing/listener.py
+++ whistle/typing/listener.py
@@ -1,6 +1,6 @@
-from typing import Any, Callable, Coroutine
+from typing import Any, Callable, Coroutine, Union
 
 from .event import IEvent
 
 # todo: async vs sync listeners to help IDEs more ?
-IListener = Callable[[IEvent], Any | Coroutine]
+IListener = Callable[[IEvent], Union[Any, Coroutine]]
